1ca05a39ab303151016048b9bf5f2a968813d010
Dzmitry Markounikau <dzmitry.markounikau@lazada.com>
2018-10-02 18:01:20 +0700

    Added settings class

--- build.sbt
+++ build.sbt
@@ -8,7 +8,6 @@ libraryDependencies ++= Seq(
     "com.qaprosoft" % "zafira-client" % "3.0.36",
     "org.scalatest" % "scalatest_2.12" % "3.0.5" % "test",
     "com.sun.jersey" % "jersey-core" % "1.19",
-    "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime
-
-
+    "ch.qos.logback" % "logback-classic" % "1.1.3" % Runtime,
+    "com.typesafe" % "config" % "1.2.1"
 )
--- src/test/scala/ZafiraReporter.scala
+++ src/test/scala/ZafiraReporter.scala
@@ -2,17 +2,12 @@ import org.scalatest.events._
 import org.scalatest._
 
 class ZafiraReporter extends Reporter {
+  val config = new Settings()
+  val zafiraUrl = config.envOrElseConfig("zafira.base.url")
+  val zafiraAdminToken = config.envOrElseConfig("zafira.admin.token")
 
-  val adminDefaultToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwicGFzc3dvcmQiOiI2RzgzSnE4dXNQQ3JocjRPVT" +
-    "ZJZnVYdC85engxSWY3NCIsInRlbmFudCI6InphZmlyYSIsImV4cCI6MTMwMzg3NDMwMjk3fQ.e97p9ZV1CWtvQm_Oo" +
-    "wEwiWbcZc8JFZELal1a7Y_4aCFHkGgXA4UUw47zDdatn3P1n7lhHp1P2NJGCpYvYrAg9g"
-  val zafiraDefaultURL = "http://demo.qaprosoft.com/zafira-ws"
-
-  val zafiraAdminToken: Option[String] = sys.env.get("ZAFIRA_ADMIN_TOKEN")
-  val zafiraUrl: Option[String] = sys.env.get("ZAFIRA_BASE_URI")
-
-  val client = new ScalaZafiraClient(zafiraUrl.getOrElse(zafiraDefaultURL))
-  val sessionToken = client.refreshToken(zafiraAdminToken.getOrElse(adminDefaultToken))
+  val client = new ScalaZafiraClient(zafiraUrl)
+  val sessionToken = client.refreshToken(zafiraAdminToken)
 
   client.setToken(sessionToken.getObject.getAccessToken)
 
