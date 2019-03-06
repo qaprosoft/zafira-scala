9fb9c8287e251da54b2c01fec9f1a0c9a7494d95
nazarenkAV <nazarenkAV@gmail.com>
2019-03-06 17:19:41 +0300

    removing conf

--- src/main/scala/com/qaprosoft/reporter/Util.scala
+++ src/main/scala/com/qaprosoft/reporter/Util.scala
@@ -24,72 +24,28 @@ import org.slf4j.ext.XLoggerFactory
 
 trait Util {
 
-  val conf = ConfigFactory.load()
-
-  private val zafira = "app.zafira."
-  private val zafiraEnabled = "zafira_enabled"
-  private val zafiraServiceUrl = "zafira_service_url"
-  private val zafiraAccessToken = "zafira_access_token"
-  private val zafiraProject = "zafira_project"
-  private val zafiraRerunFailures = "zafira_rerun_failures"
-  private val zafiraReportEmail = "zafira_report_emails"
-  private val zafiraReportShowStacktrace = "zafira_report_show_stacktrace"
-  private val zafiraConfigurator = "zafira_configurator"
+  var ZAFIRA_ENABLED = sys.props.getOrElse("zafira_enabled", true).asInstanceOf[Boolean]
+  val ZAFIRA_URL = sys.props.getOrElse("zafira_service_url", "https://stage.qaprosoft.farm/zafira-ws")
+  val ZAFIRA_ACCESS_TOKEN = sys.props.getOrElse("zafira_access_token","eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwicGFzc3dvcmQiOiJzclZmUitMVWNhVGdJbFR1SURNVmNlL1EzVVlvbmNuOCIsInRlbmFudCI6InN0YWdlIiwiZXhwIjoxMzAzODY5OTU2MjJ9.tAVXNCOCbEisbo5Uvp73w-s1XsYGtDeKL2pbYBhSCFtEgNR7CW82CSAROOdb7iPkGyCA9f-b9AtFqeBt_ICDPg")
+  val ZAFIRA_PROJECT = sys.props.getOrElse("zafira_project", "Scala")
+  var ZAFIRA_RERUN_FAILURES = sys.props.getOrElse("zafira_rerun_failures", true).asInstanceOf[Boolean]
+  val ZAFIRA_REPORT_EMAILS = sys.props.getOrElse("zafira_report_emails", false).asInstanceOf[Boolean]
+  val ZAFIRA_REPORT_SHOW_STACKTRACE = sys.props.getOrElse("zafira_report_show_stacktrace", true).asInstanceOf[Boolean]
+  val ZAFIRA_CONFIGURATOR = sys.props.getOrElse("zafira_configurator", "com.qaprosoft.zafira.config.DefaultConfigurator")
   val ZAFIRA_RUN_ID_PARAM = "zafira_run_id"
-  val ZAFIRA_PROJECT_PARAM = "zafira_project"
-  val ZAFIRA_PROPERTIES = "zafira.properties"
 
-  private val ci = "app.ci."
-  private val ciUrl = "ci_url"
-  private val ciRunId = "ci_run_id"
-  private val ciBuild = "ci_build"
-  private val ciBuildCause = "ci_build_cause"
-  private val ciParentUrl = "ci_parent_url"
-  private val ciParentBuild = "ci_parent_build"
+    var CI_URL = sys.props.getOrElse("ci_url", "https://stage.qaprosoft.com/jenkins/job/ScalaTest/job/ScalaTest/")
+    val CI_RUN_ID = sys.props.getOrElse("ci_run_id", UUID.randomUUID.toString)
+    val CI_BUILD = sys.props.getOrElse("ci_build", null)
+    val CI_BUILD_CAUSE = sys.props.getOrElse("ci_build_cause", "MANUALTRIGGER")
+    val CI_PARENT_URL = sys.props.getOrElse("ci_parent_url", null)
+    val CI_PARENT_BUILD = sys.props.getOrElse("ci_parent_build", null)
 
-  private val git = "app.git."
-  private val gitBranch = "git_branch"
-  private val gitCommit = "git_commit"
-  private val gitUrl = "git_url"
+    val GIT_BRANCH = sys.props.getOrElse("git_branch", "ddev")
+    val GIT_COMMIT = sys.props.getOrElse("git_commit", null)
+    val GIT_URL = sys.props.getOrElse("git_url", "https://github.com/qaprosoft/zafira-scala")
 
-  private val jira = "app.jira."
-  private val jiraSuiteId = "jira_suite_id"
-
-  def getStringParam(path: String, property: String) = {
-    val param = System.getProperty(property)
-    if (param != null) param
-    else conf.getString(path)
-  }
-
-  def getBooleanParam(path: String, property: String): Boolean = {
-    val param = System.getProperty(property)
-    if (param != null) param.asInstanceOf[Boolean]
-    else conf.getBoolean(path)
-  }
-
-  var ZAFIRA_ENABLED = getBooleanParam(zafira + zafiraEnabled, zafiraEnabled)
-  val ZAFIRA_URL = getStringParam(zafira + zafiraServiceUrl, zafiraServiceUrl)
-  val ZAFIRA_ACCESS_TOKEN = getStringParam(zafira + zafiraAccessToken, zafiraAccessToken)
-  val ZAFIRA_PROJECT = getStringParam(zafira + zafiraProject, zafiraProject)
-  var ZAFIRA_RERUN_FAILURES = getBooleanParam(zafira + zafiraRerunFailures, zafiraRerunFailures)
-  val ZAFIRA_REPORT_EMAILS = getStringParam(zafira + zafiraReportEmail, zafiraReportEmail)
-  val ZAFIRA_REPORT_SHOW_STACKTRACE = getBooleanParam(zafira + zafiraReportShowStacktrace, zafiraReportShowStacktrace)
-  val ZAFIRA_CONFIGURATOR = getStringParam(zafira + zafiraConfigurator, zafiraConfigurator)
-
-  var CI_URL = getStringParam(ci + ciUrl, ciUrl)
-  val CI_RUN_ID = getStringParam(ci + ciRunId, UUID.randomUUID.toString)
-  val CI_BUILD = null
-  // = getStringParam(ci + ciBuild, ciBuild)
-  val CI_BUILD_CAUSE = getStringParam(ci + ciBuildCause, ciBuildCause)
-  val CI_PARENT_URL = getStringParam(ci + ciParentUrl, ciParentUrl)
-  val CI_PARENT_BUILD = getStringParam(ci + ciParentBuild, ciParentBuild)
-
-  val GIT_BRANCH = getStringParam(git + gitBranch, gitBranch)
-  val GIT_COMMIT = getStringParam(git + gitCommit, gitCommit)
-  val GIT_URL = getStringParam(git + gitUrl, gitUrl)
-
-  var JIRA_SUITE_ID = getStringParam(jira + jiraSuiteId, jiraSuiteId)
-
+    var JIRA_SUITE_ID = sys.props.getOrElse("ci_build", null)
 
   val ciConfig: CIConfig = {
     val ci = new CIConfig
@@ -108,34 +64,6 @@ trait Util {
 
 
   val suiteLogger = XLoggerFactory.getXLogger(this.getClass)
-
-
-//    val ciConfig:CIConfig = {
-//
-//        val config:CombinedConfiguration = new CombinedConfiguration(new MergeCombiner)
-//        config.setThrowExceptionOnMissing(true)
-//        config.addConfiguration(new SystemConfiguration)
-//        config.addConfiguration(
-//          new FileBasedConfigurationBuilder[FileBasedConfiguration](classOf[PropertiesConfiguration])
-//            .configure(new Parameters().properties.setFileName(ZAFIRA_PROPERTIES))
-//            .getConfiguration)
-//
-//        val ci = new CIConfig
-//        ci.setCiRunId(config.getString("ci_run_id", UUID.randomUUID.toString))
-//        ci.setCiUrl(config.getString("ci_url", "http://localhost:8080/job/unavailable"))
-//        ci.setCiBuild(config.getString("ci_build", null))
-//        ci.setCiBuildCause(config.getString("ci_build_cause", "MANUALTRIGGER"))
-//        ci.setCiParentUrl(config.getString("ci_parent_url", null))
-//        ci.setCiParentBuild(config.getString("ci_parent_build", null))
-//
-//        ci.setGitBranch(config.getString("git_branch", null))
-//        ci.setGitCommit(config.getString("git_commit", null))
-//        ci.setGitUrl(config.getString("git_url", null))
-//
-//        JIRA_SUITE_ID = config.getString("jira_suite_id", null)
-//
-//      ci
-//    }
 
 
 }
