1d35511604ce64d8e853c22c99c1e424ff2b0938
nazarenkAV <nazarenkAV@gmail.com>
2019-03-12 15:44:28 +0300

    print run values

--- src/main/scala/com/qaprosoft/reporter/ZafiraReporter.scala
+++ src/main/scala/com/qaprosoft/reporter/ZafiraReporter.scala
@@ -146,6 +146,11 @@ class ZafiraReporter extends Reporter with Util {
           case BuildCasue.SCMTRIGGER =>
             run = zafiraClient.registerTestRunBySCHEDULER(suite.getId, convertToXML(configurator.getConfiguration), job.getId, ciConfig, Initiator.SCHEDULER, JIRA_SUITE_ID)
           case BuildCasue.MANUALTRIGGER =>
+            println("suite.getId " + suite.getId)
+            println("user.getId " + user.getId)
+            println("configurator.getConfiguration " + configurator.getConfiguration)
+            println("job.getId " + job.getId)
+
             run = zafiraClient.registerTestRunByHUMAN(suite.getId, user.getId, convertToXML(configurator.getConfiguration), job.getId, ciConfig, Initiator.HUMAN, JIRA_SUITE_ID)
           case _ =>
             throw new RuntimeException("Unable to register test run for zafira service: " + ZAFIRA_URL + " due to the misses build cause: '" + ciConfig.getCiBuildCause + "'")
