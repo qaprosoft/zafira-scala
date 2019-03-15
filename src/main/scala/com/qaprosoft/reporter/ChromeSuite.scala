946f10147f1bdfc9590b42be760174c2af58f7fa
nazarenkAV <nazarenkAV@gmail.com>
2019-03-15 19:51:17 +0300

    complitation error  fix

--- src/main/scala/com/qaprosoft/reporter/ChromeSuite.scala
+++ src/main/scala/com/qaprosoft/reporter/ChromeSuite.scala
@@ -157,7 +157,6 @@ object WebDriverPool {
 
   override def withFixture(test: NoArgTest) = {
     var outcome:Outcome = null
-    if ()
     if(RUN_TESTS.contains(test.name)) {
       println(test.name + " is applyed")
       outcome = super.withFixture(test)
